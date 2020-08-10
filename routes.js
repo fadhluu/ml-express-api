4f6e84f444ed9a2d24ced3f46a8374a26d1375c6
fadhlu <fadhlurahman.i@gmail.com>
2020-08-10 19:44:20 +0700

    add package

--- routes.js
+++ routes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const routes = express.Router();
 const heroes = require('./list_hero.json');
 
+const capitalize = s => {
+  return s[0].toUpperCase() + s.slice(1);
+};
+
 routes.get('/heroes', (req, res) => {
   res.status(200).json({ message: 'List Heroes', data: heroes });
 });
 
 routes.get('/heroes/:hero_name', (req, res) => {
-  const hero_name = req.params.hero_name;
+  let hero_name = req.params.hero_name;
+  hero_name = capitalize(hero_name);
 
   const hero = require(`./heroes/${hero_name}.json`);
   res.status(200).json({ data: hero });
