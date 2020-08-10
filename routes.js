33ca18cd3e3316a15773ee92bd15ade8f4177a15
fadhlu <fadhlurahman.i@gmail.com>
2020-08-10 17:42:40 +0700

    init

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = express.Router();
+const heroes = require('./list_hero.json');
+
+routes.get('/heroes', (req, res) => {
+  res.status(200).json({ message: 'List Heroes', data: heroes });
+});
+
+routes.get('/heroes/:hero_name', (req, res) => {
+  const hero_name = req.params.hero_name;
+
+  const hero = require(`./hero/${hero_name}.json`);
+  res.status(200).json({ data: hero });
+});
+
+module.exports = routes;
