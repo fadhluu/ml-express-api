0ae8fc62795653fb8e27c5824821cfdf5c25c0a5
fadhlu <fadhlurahman.i@gmail.com>
2020-08-10 19:55:58 +0700

    fix port

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 const routes = require('./routes');
 
 // app.use(express.json());
 app.use(express.static('public'));
 app.use('/', routes);
 
-app.listen(port, () => console.log(`Server running in ${port}`));
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server running in ${port}`)
+);
